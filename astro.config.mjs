c41f53f13257928c2cf872ef3cbc321ca04e32af
griffinjoshs <leveloutusa@gmail.com>
2025-09-26 14:33:34 -0400

    vercel speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from 'astro-icon';
 import { iconConfig } from './iconsConfig.js';
 
 import partytown from '@astrojs/partytown';
+import vercel from '@astrojs/vercel/static';
 
 // Load environment variables from .env files
 const env = loadEnv(
